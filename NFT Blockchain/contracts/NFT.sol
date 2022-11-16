8688e7610d6552cb105bb76876bfe30fd1074f7e
Konstantinos Papageorgiou <kwnstantinospapageorgiou@gmail.com>
2022-11-16 14:31:35 +0200

    16/11/22

---
+++ NFT Blockchain/contracts/NFT.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFT is ERC721URIStorage, Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT", "ENEFTI"){}
+
+    function mintNFT(address _signer, bytes memory _imageb, bytes memory _signature) public returns (uint256){
+        bytes32 imageHash = getMessageHash(_imageb);
+        bytes32 ethSignedImageHash = getEthSignedDataHash(imageHash);
+
+        require(recover(ethSignedImageHash, _signature) == _signer, "Invalid signature");
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(_signer, newItemId);
+        return newItemId;
+
+    }
+
+    function getMessageHash(bytes memory _data) public pure returns (bytes32){
+        return keccak256(abi.encodePacked(_data));
+    }
+
+    function getEthSignedDataHash(bytes32 _dataHash) public pure returns (bytes32){
+        return keccak256(abi.encodePacked(
+            "\x19Ethereum Signed Message:\n32",
+            _dataHash
+        ));
+    }
+
+    function recover(bytes32 _ethSignedDataHash, bytes memory _signature) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = _split(_signature);
+        return ecrecover(_ethSignedDataHash, v, r, s);
+    }
+
+    function _split(bytes memory _signature) internal pure returns (bytes32 r, bytes32 s, uint8 v){
+    
+        require(_signature.length == 65, "Invalid signature length");
+
+        assembly{
+            r := mload(add(_signature, 32))
+            s := mload(add(_signature, 64))
+            v := byte(0, mload(add(_signature, 96)))
+        }
+    }
+}
